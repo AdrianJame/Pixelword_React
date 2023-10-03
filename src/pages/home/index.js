b42906f20577eb444f6717dde3c211e9995f9692
AdrianJame <adrianjamesbadwolf@gmail.com>
2023-10-03 18:27:41 -0300

    quase pronto

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,7 +1,101 @@
+import { useState, useEffect } from 'react';
 import './index.scss';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { confirmAlert } from 'react-confirm-alert';
 
 export default function PixelHome(){
+    const[erro , setErro] = useState('')
+    const[listar, setListar] = useState([])
+
+    const[marca , setMarca] = useState('')
+    const[nome , setNome] = useState('')
+    const[estoque , setEstoque] = useState('')
+    const[preco , setPreco] = useState('')
+    const[garantia , setGarantia] = useState('')
+    const[categoria , setCategoria] = useState(0)
+    const[id, setId] = useState(0)
+
+    async function limpar(){
+        setCategoria(0)
+        setMarca('')
+        setNome('')
+        setEstoque('')
+        setPreco('')
+        setGarantia('')
+        setId(0)
+      }
+
+    async function Listar(){
+        let r = await axios.get('http://localhost:5000/listar')
+        setListar(r.data)
+    }
+
+    useEffect(() => {
+        Listar()
+    }, [])
+
+    async function Alterar(item){
+        setCategoria(0)
+        setMarca(item.nm_marca)
+        setNome(item.nm_produto)
+        setEstoque(item.ds_estoque)
+        setPreco(item.nr_preco)
+        setGarantia(item.nr_garantia)
+        setId(item.id_produto)
+    }
+
+    async function Salvar(){
+            try{
+                let produto = {
+                    categoria: categoria,
+                    marca: marca,
+                    nome: nome,
+                    preco: preco,
+                    estoque: estoque,
+                    garantia: garantia
+                }
+    
+                if(id == 0){
+                    let r = await axios.post('http://localhost:5000/inserir', produto)
+                    setErro('Produto adicionado')
+                }
+    
+                else if(id != 0){
+                    let r = await axios.put('http://localhost:5000/alterar/' + id, produto);
+                    setErro('Produto alterado')
+                }
+    
+                limpar()
+    }
+    catch(err){
+      setErro(err.response.data.erro)
+    }
+  }
+
+
+  async function Deletar(id){
+    confirmAlert({
+      title: 'PRODUTOS',
+      message: 'Tem certeza que deseja remover?',
+      buttons: [
+        {
+          label: 'Sim',
+          onClick: async () => {
+            let r = await axios.delete('http://localhost:5000/deletar/' + id);
+            alert('Veículo foi removido com sucesso');
+            Listar()
+          }
+        },
+        {
+          label: 'Não'
+        }
+      ]
+    });
+    
+  } 
+
+
 
     return(
         <div className='pagina-pixelhome'>
@@ -45,25 +139,32 @@ export default function PixelHome(){
 
                 <div className='s2-cadastro'>
                     <section className='s2-inputs'>
-                        <select>
+                        <select value={categoria} onChange={e => setCategoria(e.target.value)}>
                             <option>CATEGORIAS</option>
+                            {listar.map(item =>
+                                <option>{item.nm_categoria}</option>
+                            )}
                         </select>
 
-                        <input placeholder='MARCA'/>
-                        <input placeholder='NOME DO PRODUTO'/>
+                        <input placeholder='MARCA' value={marca} onChange={e => setMarca(e.target.value)}/>
+                        <input placeholder='NOME DO PRODUTO' value={nome} onChange={e => setNome(e.target.value)}/>
                     </section>
 
                     <section className='s2-linha'></section>
 
                     <section className='s2-inputs'>
-                        <input placeholder='ESTOQUE'/>
-                        <input placeholder='PREÇO'/>
-                        <input placeholder='GARANTIA'/>
+                        <input placeholder='ESTOQUE' value={estoque} onChange={e => setEstoque(e.target.value)}/>
+                        <input placeholder='PREÇO' value={preco} onChange={e => setPreco(e.target.value)}/>
+                        <input placeholder='GARANTIA' value={garantia} onChange={e => setGarantia(e.target.value)}/>
                     </section>
 
                 </div>
 
-                <Link>Salvar</Link>
+                <button onClick={Salvar}>
+                  {id == 0 ? 'Salvar' : 'Alterar'}
+              </button>
+
+                <p>{erro}</p>
             </section>
 
             <section className='pixelhome-s3'>
@@ -106,7 +207,22 @@ export default function PixelHome(){
                 </section>
                 <div className='s3-linha'></div>
 
-                {/* aqui vai ficar o map */}
+                {listar.map(item =>
+                    <section className='s3-tabela'>
+                        <div className='tabela'>
+                            <p>{item.nm_categoria}</p>
+                            <p>{item.nm_marca}</p>
+                            <p>{item.nm_produto}</p>
+                            <p>{item.ds_estoque}</p>
+                            <p>{item.nr_preco}</p>
+                            <p>{item.nr_garantia}</p>
+
+                            <img src='/assets/images/alterar.svg' onClick={() => Alterar(item)}/>
+                            <img src='/assets/images/deletar.svg' onClick={() => Deletar(item.id_produto)}/>
+                        </div>
+                        <section className='linha-tabela'></section>
+                    </section>
+                )}
             </section>
         </div>
     )
